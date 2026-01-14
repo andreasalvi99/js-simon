eef6d4479dff6a977ed7fec9af99411024c79b3b
andreasalvi99 <andrea.salvi.r@gmail.comm>
2026-01-14 17:23:05 +0100

    setted interval and added to the html

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 // ?? Recupero gli elemtni HTML
 const numbersDisplay = document.getElementById("numbers-list");
+const countdown = document.getElementById("countdown");
 
 // ?? Genero 5 numeri casuali che vanno sia mostrati all'utente, sia inseriti in un array vuoto
 const randomNumbers = [];
@@ -11,3 +12,21 @@ for (let i = 0; i < 5; i++) {
   numbersDisplay.innerText = randomNumbers.join(" ");
 }
 console.log(randomNumbers);
+
+// ?? Imposto il countdown
+let countdownSec = 11;
+
+const intervalId = setInterval(countDownTimer, 1000);
+countDownTimer();
+
+function countDownTimer() {
+  countdownSec--;
+  console.log(countdownSec);
+  countdown.innerText = countdownSec;
+
+  if (countdownSec <= 0) {
+    clearInterval(intervalId);
+  }
+}
+
+// ?? Aggiungo il countdown in HTML
