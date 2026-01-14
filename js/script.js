120eb8db88d1c6aface57b7d49f5ab9c5de309ef
andreasalvi99 <andrea.salvi.r@gmail.comm>
2026-01-14 16:56:04 +0100

    displayed numbers in html

--- js/script.js
+++ js/script.js
@@ -1,10 +1,13 @@
-// ?? Genero 5 numeri casuali che vanno sia mostrati all'utente, sia inseriti in un array vuoto
+// ?? Recupero gli elemtni HTML
+const numbersDisplay = document.getElementById("numbers-list");
 
+// ?? Genero 5 numeri casuali che vanno sia mostrati all'utente, sia inseriti in un array vuoto
 const randomNumbers = [];
-console.log(randomNumbers);
 
 for (let i = 0; i < 5; i++) {
   const randomNumber = Math.floor(Math.random() * 100 + 1);
   console.log(randomNumber);
   randomNumbers.push(randomNumber);
 }
+numbersDisplay.innerText = `${randomNumbers}`;
+console.log(randomNumbers);
